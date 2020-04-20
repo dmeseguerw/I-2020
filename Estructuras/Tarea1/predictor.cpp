70924181d2d0b019c08f454198f6de6ff3cb587a
Daniel Meseguer <daniel@meseguer.biz>
2020-04-20 14:29:20 -0600

    Agrego funcion que calcula global y bimodal en uno solo

--- Estructuras/Tarea1/predictor.cpp
+++ Estructuras/Tarea1/predictor.cpp
@@ -5,14 +5,14 @@
 
 using namespace std;
 
-string convert_state(int number){
+string convert_state(int number){ //Convierte estados en binario a taken o not taken
     string real_state;
     if (number >= 2){real_state = "T";}
     else{real_state = "N";}
     return real_state;
 }
 
-string print_states(vector<int> contadores){
+string print_states(vector<int> contadores){ //Imprime estados en weakly y strongly
     string states = "";
     string current = "";
     for (int i=0;i<contadores.size();i++){
@@ -26,51 +26,80 @@ string print_states(vector<int> contadores){
     return states;
 }
 
-void bimodal(int s){
 
-    string ln;
-    vector<int> contadores((int)(pow(2.0,(double)s)), 00);
-    string prediccion;
-    int mask = (int)(pow(2.0,(double)s)) - 1;
-    int last_bits;
+int get_state(string line, int bp, int history, int s){ //Obtiene el estado que se debe utilizar
     int state;
-    string real;
-    string pred;
-    string taken = "T";
-    string ntaken = "N";
-    string CorI = "";
-    int counter = 0;
-    while(getline(cin, ln)){
-        counter++;
-        if(counter == 100){break;}
-        last_bits = atoi(&ln[0]) & mask;
-        real = &ln[11];
-        pred = convert_state(contadores[last_bits]);
-        if(pred == real){CorI = "Correct";}
-        else{CorI = "Incorrect";}
-        cout << last_bits << " | " << print_states(contadores) << " | " << pred << " | " << real << " | " << CorI << endl;
+    int mask = (int)(pow(2.0,(double)s)) - 1;
+    if(bp == 0) state =  (stol(line.substr(0, line.find(" ")))) & mask;
+    else if(bp == 2) state = (stol(line.substr(0, line.find(" "))) ^ history) & mask;
+    return state;
+}
 
-        if(real == "T"){
-            if(contadores[last_bits]<3)
-            {contadores[last_bits]++;}
-        }
-        if(real == "N"){
-            if(contadores[last_bits]>0){
-                contadores[last_bits] = contadores[last_bits]-1;
-            }
-        }
+//Decide si la prediccion es correcta o incorrecta
+string make_prediction(string predicted, string real){
+    string prediction;
 
-        cout << contadores[last_bits] << endl;
+    if(predicted == real) prediction = "Correct";
+    else prediction = "Incorrect";
+
+
+    return prediction;
+}
+
+vector<int> update_contadores(vector<int> contadores, int state, string real){//Hace update a los contadores de dos bits
+    if(real == "T"){
+        if(contadores[state]<3) contadores[state]++;
     }
+
+    if(real == "N"){
+        if(contadores[state]>0) contadores[state] = contadores[state]-1;
+    }
+
+    return contadores;
+}
+
+int update_history(int history, int mask, string real){//Hace update a los bits de historia para el global share
+    if(real == "T") history = ((history<<1) | 1) & mask;
+    else if(real == "N") history = (history<<1) & mask;
 }
 
 
+
+
+
+void gshare_and_bimodal(int s, int gh, int bp){
+    string ln;
+    vector<int> contadores((int)(pow(2.0,(double)s)), 00);
+    string real;
+    string predicted, prediction;
+    int state;
+    int mask_for_h = (int)(pow(2.0,(double)gh)) - 1;
+    int mask = (int)(pow(2.0,(double)s)) - 1;
+    int history = 0*gh;
+    int counter = 0;
+    while(getline(cin,ln)){
+        counter++;
+        if(counter == 100){break;}
+
+        state = get_state(ln, bp, history, s);
+        real = ln.back();
+        predicted = convert_state(contadores[state]);
+        prediction = make_prediction(predicted, real);
+        cout << print_states(contadores) << " | " << predicted << " | " << real << " | " << prediction << endl;
+        
+        
+        contadores = update_contadores(contadores, state, real);
+        if(bp == 2) history = update_history(history, mask_for_h, real);
+    }
+
+
+}
 
 void tournament(){}
 
 void pshare(){}
 
-void gshare(){}
+
 
 void create_table(){}
 
@@ -83,9 +112,9 @@ int main(int argc, char **argv){
     // Obtencion de parametros
     int s, bp, gh, ph, o;
     s = atoi(argv[2]);
-    bimodal(s);
-    // bp = atoi(argv[4]);
-    // gh = atoi(argv[6]);
+    bp = atoi(argv[4]);
+    gh = atoi(argv[6]);
+    gshare_and_bimodal(s, gh, bp);
     // ph = atoi(argv[8]);
     // o = atoi(argv[10]);
 
