3840d8c84da3285a3770d448f6500c3d501f4f5c
Daniel Meseguer <daniel@meseguer.biz>
2020-06-26 18:48:54 -0600

    Agrego script

---
+++ Proyecto/Programado/install_dep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get install sox
+apt-get install python3
+apt-get install python3-matplotlib
+apt-get install python3-numpy
+apt-get install python3-pil
+apt-get install python3-tk
+apt-get install python3-pandas
